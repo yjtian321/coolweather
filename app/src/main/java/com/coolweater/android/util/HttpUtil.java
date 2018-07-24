8ebb898d3a36b8b0406bf1afd9b0b2b9ecef1fd0
tianyj <tianyj@gionee.com>
2018-07-24 18:14:10 +0800

    完成遍历省市县三级列表的功能
    Change-Id: I4fc7d91f6bd279d6a4fbf4d29da9055e891dbdb5

---
+++ app/src/main/java/com/coolweater/android/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.coolweater.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by tianyj on 7/23/18.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
